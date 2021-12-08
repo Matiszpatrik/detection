60780183f938739095098a06f51c383d922d18d6
matis <matisz.patrik@gmail.com>
2021-12-08 17:16:15 +0100

    minta

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def draw_the_lines (img,lines):
 
     for line in lines:
         for x1, y1, x2, y2 in line:
-            cv2.line(blank_image, (x1, y1), (x2, y2), (255,0,0), thickness=10)
+            cv2.line(blank_image, (x1, y1), (x2, y2), (0,255,0), thickness=10)
 
     img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
     return img
 
-image = cv2.imread('car_4.jpg')
+image = cv2.imread('car.jpg')
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 #print(image.shape)
@@ -29,23 +29,23 @@ height = image.shape[0]
 width = image.shape[1]
 
 region_of_interest_vertices = [
-    (1200, height),
-    (1200, 0),
+    (1500, height),
+    (1500, 0),
     (2000, 0),
     (2000, height)
 ]
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-canny_image = cv2.Canny(gray_image, 100, 200)
+canny_image = cv2.Canny(gray_image, 100, 120)
 cropped_image = region_of_interest(canny_image, np.array([region_of_interest_vertices], np.int32))
 
 lines = cv2.HoughLinesP(cropped_image,
-                        rho=6,
+                        rho=2,
                         theta=np.pi/60,
-                        threshold=160,
+                        threshold=50,
                         lines=np.array([]),
                         minLineLength=40,
-                        maxLineGap=25)
+                        maxLineGap=100)
 
 image_with_lines = draw_the_lines(image, lines)
 
